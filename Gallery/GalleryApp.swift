9f1cf5e561d7a90cfc35839672e5686aeb6fd1d8
32Moon <lmj970302@gmail.com>
2022-05-02 15:12:54 +0900

    ✨프로젝트 생성

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by 이문정 on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
